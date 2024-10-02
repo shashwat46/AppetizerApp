3116f18d9b70e4a3dc46140b83e6a4a7d56a36ab
shashwatsingh <shashwat.singh0406@gmail.com>
2024-10-03 02:15:56 +0530

    feat: created Network Call to download images for AppetizerList

--- Appetizer/NetworkManager.swift
+++ Appetizer/NetworkManager.swift
@@ -5,16 +5,18 @@
 //  Created by Shashwat Singh on 2/10/24.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager{
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
     
     private init(){}
     
+    //MARK: getAppetizers
     func getAppetizers(completed : @escaping(Result<[Appetizer], APError>) -> Void){
         guard let url = URL(string: appetizerURL) else {
             completed(.failure(.invalidURL))
@@ -47,6 +49,38 @@ final class NetworkManager{
                 completed(.failure(.invalidData))
             }
             
+        }
+        
+        task.resume()
+        
+    }//getAppetizer() ends
+    
+    
+    //MARK: downloadImage
+    func downloadImage(fromURLString urlString : String, completed : @escaping(UIImage?) -> Void){
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey){
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else{
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url : url))
+        { data, response, error in
+            
+            guard let data = data, let image = UIImage(data: data) else{
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         
         task.resume()
