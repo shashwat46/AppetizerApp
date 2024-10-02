4a58e9a4893ad3db62603ccdcd9d73ec1b2d12e2
shashwatsingh <shashwat.singh0406@gmail.com>
2024-10-02 22:21:13 +0530

    feat : Configured Network Manager to parse API response with error handling

---
+++ Appetizer/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizer
+//
+//  Created by Shashwat Singh on 2/10/24.
+//
+
+import Foundation
+
+enum APError : Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
