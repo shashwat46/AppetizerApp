a6c0bcbac7ae68d047a3de8bc950d040f2a18065
shashwatsingh <shashwat.singh0406@gmail.com>
2024-10-09 20:16:00 +0530

    refactor: Refactored AppetizerDetailView

--- Appetizer/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizer/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -16,6 +16,8 @@ struct AppetizerDetailView: View {
         
         VStack{
             AppetizerRemoteImage(urlString: appetizer.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225)
                 
             VStack{
                 Text(appetizer.name)
@@ -28,39 +30,9 @@ struct AppetizerDetailView: View {
                     .padding()
                 
                 HStack(spacing:40){
-                    VStack(spacing: 5){
-                        Text("Calories")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.calories)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-
-                    }
-                    VStack(spacing: 5){
-                        Text("Carbs")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.carbs)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-
-                    }
-                    VStack(spacing: 5){
-                        Text("Protein")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.protein)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-
-                    }
+                    NutritionInfo(title: "Calories", value: appetizer.calories)
+                    NutritionInfo(title: "Carbs", value: appetizer.carbs)
+                    NutritionInfo(title: "Protein", value: appetizer.protein)
                 }
             }
             
@@ -69,13 +41,7 @@ struct AppetizerDetailView: View {
             Button{
                 print("Tapped")
             }label: {
-                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                    .frame(width: 260, height: 50)
-                    .foregroundColor(.white)
-                    .background(Color.brandPrimary)
-                    .cornerRadius(10)
+                APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
             }
             .padding(.bottom, 30)
         }
@@ -86,17 +52,7 @@ struct AppetizerDetailView: View {
         .overlay(Button{
             isShowingDetail = false
         } label: {
-            ZStack{
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.white)
-                    .opacity(0.6)
-                
-                Image(systemName: "xmark")
-                    .imageScale(.small)
-                    .frame(width: 44, height: 44)
-                    .foregroundColor(.black)
-            }
+            XDismissButton()
         }, alignment: .topTrailing)
     }
 }
@@ -104,5 +60,25 @@ struct AppetizerDetailView: View {
 struct AppetizerDetailView_Previews: PreviewProvider {
     static var previews: some View {
         AppetizerDetailView(appetizer: MockData.sampleAppetizer, isShowingDetail: .constant(true))
+    }
+}
+
+struct NutritionInfo : View{
+    
+    let title: String
+    let value: Int
+    
+    var body: some View{
+        VStack(spacing: 5){
+            Text(title)
+                .bold()
+                .font(.caption)
+            
+            Text("\(value)")
+                .foregroundColor(.secondary)
+                .fontWeight(.semibold)
+                .italic()
+
+        }
     }
 }
