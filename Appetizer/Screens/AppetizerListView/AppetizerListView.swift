c8624f89011ab15c1807c8462ac00babaf3940f0
shashwatsingh <shashwat.singh0406@gmail.com>
2024-10-09 20:21:41 +0530

    refactor: Refactored DetailView related vars to ViewModel

--- Appetizer/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizer/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,30 +10,28 @@ import SwiftUI
 struct AppetizerListView: View {
     
     @StateObject var viewModel = AppetizerListViewModel()
-    @State private var isShowingDetail = false
-    @State private var selectedAppetizer : Appetizer?
-    
+
     var body: some View {
         ZStack{
             NavigationView{
                 List(viewModel.appetizers){appetizer in
                     AppetizerListCell(appetizer: appetizer)
                         .onTapGesture {
-                            selectedAppetizer = appetizer
-                            isShowingDetail = true
+                            viewModel.selectedAppetizer = appetizer
+                            viewModel.isShowingDetail = true
                         }
                 }
                 .navigationTitle("🍱 Appetizers")
-                .disabled(isShowingDetail)
+                .disabled(viewModel.isShowingDetail)
                 .listStyle(.plain)
             }
             .onAppear{
                 viewModel.getAppetizers()
             }
-            .blur(radius: isShowingDetail ? 20 : 0)
+            .blur(radius: viewModel.isShowingDetail ? 20 : 0)
             
-            if isShowingDetail{
-                AppetizerDetailView(appetizer: selectedAppetizer!, isShowingDetail: $isShowingDetail)
+            if viewModel.isShowingDetail{
+                AppetizerDetailView(appetizer: viewModel.selectedAppetizer!, isShowingDetail: $viewModel.isShowingDetail)
             }
             
             if viewModel.isLoading{
