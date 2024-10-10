167250a7843c1c1b18e5605662416342d0c12efe
shashwatsingh <shashwat.singh0406@gmail.com>
2024-10-10 18:14:53 +0530

    feat: FocusState for Account form

--- Appetizer/Screens/AccountView/AccountView.swift
+++ Appetizer/Screens/AccountView/AccountView.swift
@@ -10,14 +10,30 @@ import SwiftUI
 struct AccountView: View {
     
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField{
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView{
             Form{
                 Section(header: Text("Personal Info")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit {focusedTextField = .lastName}
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit {focusedTextField = .email}
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit {focusedTextField = nil}
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
